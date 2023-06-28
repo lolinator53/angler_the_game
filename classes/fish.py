378859d73375af2e7ad05bbde082a4cd62991893
lolinator53 <lolinator53@gmail.com>
2023-06-29 01:39:55 +0200

    added classes for player and fish

---
+++ classes/fish.py
@@ -0,0 +1,6 @@
+class Fish(object):
+    def __init__(self, name, rarity, weight, worth):
+        self.name = name
+        self.rarity = rarity
+        self.weight = weight
+        self.worth = worth
---
+++ classes/player.py
@@ -0,0 +1,5 @@
+class Player:
+
+    def __init__(self, inventory, wallet):
+        self.inventory = inventory
+        self.wallet = wallet
